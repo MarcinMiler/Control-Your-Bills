cb9821299e0a53a756bea4b15c9fce017064ceb4
Marcin Miler <marcin4951@gmail.com>
2018-08-04 23:51:52 +0200

    Layout component

---
+++ client/src/Components/Layout/index.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+import { Container } from './style'
+
+export const Layout: React.SFC = ({ children }) => (
+    <Container>{children}</Container>
+)
--- client/src/Routes/index.tsx
+++ client/src/Routes/index.tsx
@@ -3,14 +3,18 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 import { Sidebar } from '../Components/Sidebar'
 import { Dashboard } from '../Views/Dashboard'
+import { Layout } from '../Components/Layout'
 
 export const Routes = () => (
     <BrowserRouter>
         <>
             <Sidebar />
-            <Switch>
-                <Route exact path="/" component={Dashboard} />
-            </Switch>
+
+            <Layout>
+                <Switch>
+                    <Route exact path="/" component={Dashboard} />
+                </Switch>
+            </Layout>
         </>
     </BrowserRouter>
 )
