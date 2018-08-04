90a15db920e38fd14f003f43bf7de4543f336281
Marcin Miler <marcin4951@gmail.com>
2018-08-04 23:31:59 +0200

    Added globals styles and theme

---
+++ client/src/Theme/index.ts
@@ -0,0 +1,6 @@
+export const theme = {
+    pink: '#E64BD8;',
+    darkblue: '#21346A',
+    lightgray: '#F1F1F6',
+    darkgray: '#C9C9C9'
+}
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,29 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { injectGlobal } from 'styled-components'
 
 import { Routes } from './Routes'
 import registerServiceWorker from './registerServiceWorker'
+import { ThemeProvider } from 'styled-components'
+import { theme } from './Theme'
 
-ReactDOM.render(<Routes />, document.getElementById('root') as HTMLElement)
+// tslint:disable-next-line
+injectGlobal`
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:400,500,600');
+
+    body {
+        margin: 0px;
+        font-family: Montserrat;
+    }
+    p,h1,h2,h3,h4,h5,h6 {
+        margin: 0;
+    }
+`
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <Routes />
+    </ThemeProvider>,
+    document.getElementById('root') as HTMLElement
+)
 registerServiceWorker()
